202e96620ff4152dff56b90859e28cd4a736b6de
amuolo <and.muolo@gmail.com>
2024-02-22 11:10:38 +0100

    cleanups

--- App/Controllers/LogsController.cs
+++ App/Controllers/LogsController.cs
@@ -1,6 +1,7 @@
 ﻿using App.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Model;
 
 namespace App.Controllers;
 
@@ -13,8 +14,15 @@ public class LogsController : Controller
         _dbContext = dbContext;
     }
 
+    // GET: Transactions
     public async Task<IActionResult> Index()
     {
         return View(await _dbContext.ActivityLogs.ToListAsync());
+    }
+
+    // GET: Transactions/List
+    public async Task<List<ActivityLog>> List()
+    {
+        return await _dbContext.ActivityLogs.ToListAsync();
     }
 }
