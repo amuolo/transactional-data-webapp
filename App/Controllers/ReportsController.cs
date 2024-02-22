19c35bd701a6d8a5a97ba5920f4b50515cca7da6
amuolo <and.muolo@gmail.com>
2024-02-22 10:32:50 +0100

    cleanups

---
+++ App/Controllers/ReportsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Controllers;
+
+public class ReportsController : Controller
+{
+    public IActionResult Reports()
+    {
+        return View();
+    }
+}
